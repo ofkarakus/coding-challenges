589aaed9b7aa80876e4bd3eedba158f193cf8e84
ofkarakus <omerfarukkarakus01@gmail.com>
2020-10-17 09:30:55 +0100

    fix: correct .py code

--- javascript/cc-003/reverseWords.js
+++ javascript/cc-003/reverseWords.js
@@ -2,7 +2,8 @@ const reverseWords = (sentence) => {
   return sentence.split(" ").reverse().join(" ");
 };
 
-console.log(reverseWords("days. big make things Little"));
-console.log(
-  reverseWords("impossible. mean not does hard but hard, be to going It’s")
-);
+
+console.log(reverseWords("impossible. mean not does hard but hard, be to going It’s"))
+console.log(reverseWords("days. big make things Little"))
+console.log(reverseWords("done. you’re when Stop tired. you’re when stop Don’t"))
+console.log(reverseWords("Clarusway"))
--- javascript/cc-003/reverseWords.py
+++ javascript/cc-003/reverseWords.py
@@ -1,6 +1,8 @@
 def reverseWords(sentence):
-    sentList = sentence.split()
-    return ' '.join(sorted(sentList, key=sentList.index, reverse=True))
+    return ' '.join(sentence.split()[-1::-1])
 
 
 print(reverseWords("impossible. mean not does hard but hard, be to going It’s"))
+print(reverseWords("days. big make things Little"))
+print(reverseWords("done. you’re when Stop tired. you’re when stop Don’t"))
+print(reverseWords("Clarusway"))
